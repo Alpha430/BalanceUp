5f3ff0e05efa96fe1db7186e90a1df28aabad1f2
abhis <shindeabhishek100@gmail.com>
2026-02-07 12:56:09 +0530

    Updated UI

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,4 @@
         alias(libs.plugins.kotlin.android) apply false
         alias(libs.plugins.hilt.android) apply false
         alias(libs.plugins.google.services) apply false
-    }
+    }   
