f999a33d2a14bbe01cf3e2c0ba7f26e8f4724b25
abhis <shindeabhishek100@gmail.com>
2026-03-27 16:41:35 +0530

    Upgrade Room version from 2.6.1 to 2.7.0-alpha13

--- app/src/main/java/com/alpha/balanceup/data/local/dao/UserDao.kt
+++ app/src/main/java/com/alpha/balanceup/data/local/dao/UserDao.kt
@@ -2,13 +2,12 @@ package com.alpha.balanceup.data.local.dao
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
-import androidx.room3.OnConflictStrategy
 import com.alpha.balanceup.data.local.entity.UserEntity
 
 @Dao
 interface UserDao {
-
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insert(user: UserEntity)
 
