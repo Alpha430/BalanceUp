47b4e3577aad3e483e94502cc9eb7b3afc975a23
abhis <shindeabhishek100@gmail.com>
2026-05-23 16:24:59 +0530

    Implemented AddExpenseActivity UI and added layout resources.

--- app/src/main/java/com/alpha/balanceup/ui/dashboard/AddExpenseActivity.kt
+++ app/src/main/java/com/alpha/balanceup/ui/dashboard/AddExpenseActivity.kt
@@ -1,6 +1,8 @@
 package com.alpha.balanceup.ui.dashboard
 
 import android.os.Bundle
+import android.view.animation.AnimationUtils
+import com.alpha.balanceup.R
 import com.alpha.balanceup.core.base.BaseActivity
 import com.alpha.balanceup.databinding.ActivityAddExpenseBinding
 
@@ -14,8 +16,24 @@ class AddExpenseActivity : BaseActivity() {
         setContentView(binding.root)
         setupEdgeToEdge(binding.root)
 
+        setupListeners()
+    }
+
+    private fun setupListeners() {
         binding.toolbar.setNavigationOnClickListener {
             onBackPressedDispatcher.onBackPressed()
         }
+
+        binding.btnBalanceUp.setOnClickListener {
+            val anim = AnimationUtils.loadAnimation(this, R.anim.btn_click)
+            it.startAnimation(anim)
+            
+            // Logic to handle Balance Up can be added here
+            handleBalanceUp()
+        }
+    }
+
+    private fun handleBalanceUp() {
+        // TODO: Save data and navigate or show result
     }
 }
